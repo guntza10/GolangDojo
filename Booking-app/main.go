9a2150236fb4c6de0a607e573119a4417930660a
guntza10 <jomphop.saibuatong@hotmail.com>
2023-01-12 14:47:34 +0700

    update main.go

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -3,33 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	conferenceName := "Go Conference"
-	const conferenceTickets = 50
+	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	conferenceName := "Go Conference"
+	bookings := []string{}
+
 	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
-	fmt.Println("=================================")
 
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
 
-	// fmt.Scan passing the reference address in memory not value (ต้องมี & pointer อยู่ด้านหน้า value เพื่อ ref ถึง address ใน memory)
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	// asking for user input
+	fmt.Println("Enter Your First Name: ")
+	fmt.Scanln(&firstName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+	fmt.Println("Enter Your Last Name: ")
+	fmt.Scanln(&lastName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+	fmt.Println("Enter Your Email: ")
+	fmt.Scanln(&email)
 
 	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+	fmt.Scanln(&userTickets)
 
+	// book ticket in system
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Println("=================================")
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
